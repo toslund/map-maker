8399727ff512642f2f064e803eae6fe06cbc0e28
toslund <timoslund@gmail.com>
2022-01-21 08:05:31 -0800

    indenting

--- src/components/MapMaker.vue
+++ src/components/MapMaker.vue
@@ -300,14 +300,14 @@ export default {
       colorSchemesFamily: 0,
       activeColorScheme:
         {
-            name: 'Reds',
-            colors: {
-              3: ['#fee8c8', '#fdbb84', '#e34a33'],
-              4: ['#fef0d9', '#fdcc8a', '#fc8d59', '#d7301f'],
-              5: ['#fef0d9', '#fdcc8a', '#fc8d59', '#e34a33', '#b30000'],
-              6: ['#fef0d9', '#fdd49e', '#fdbb84', '#fc8d59', '#e34a33', '#b30000'],
-            },
+          name: 'Reds',
+          colors: {
+            3: ['#fee8c8', '#fdbb84', '#e34a33'],
+            4: ['#fef0d9', '#fdcc8a', '#fc8d59', '#d7301f'],
+            5: ['#fef0d9', '#fdcc8a', '#fc8d59', '#e34a33', '#b30000'],
+            6: ['#fef0d9', '#fdd49e', '#fdbb84', '#fc8d59', '#e34a33', '#b30000'],
           },
+        },
       colorSchemes: {
         sequential: [
           {
