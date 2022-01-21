af8e35a71b6a40107a58f647f143267159aaa104
toslund <timoslund@gmail.com>
2022-01-21 07:58:16 -0800

    change default colors

--- src/components/MapMaker.vue
+++ src/components/MapMaker.vue
@@ -290,7 +290,7 @@ export default {
         },
       },
       colors: {
-        background: { color: '#b3f0ff', name: 'Background', key: 'background' },
+        background: { color: '#0A0A0A', name: 'Background', key: 'background' },
         noData: { color: '#27251b', name: 'No Data', key: 'noData' },
         primaryStroke: { color: '#111111', name: 'Primary Stroke', key: 'primaryStroke' },
         focus: { color: '#ADACAA', name: 'Focus/Hover', key: 'focus' },
@@ -300,16 +300,14 @@ export default {
       colorSchemesFamily: 0,
       activeColorScheme:
         {
-          name: 'Greens',
-          colors: {
-            1: ['#e5f5f9'],
-            2: ['#e5f5f9', '#99d8c9'],
-            3: ['#e5f5f9', '#99d8c9', '#2ca25f'],
-            4: ['#edf8fb', '#b2e2e2', '#66c2a4', '#238b45'],
-            5: ['#edf8fb', '#b2e2e2', '#66c2a4', '#2ca25f', '#006d2c'],
-            6: ['#edf8fb', '#ccece6', '#99d8c9', '#66c2a4', '#2ca25f', '#006d2c'],
+            name: 'Reds',
+            colors: {
+              3: ['#fee8c8', '#fdbb84', '#e34a33'],
+              4: ['#fef0d9', '#fdcc8a', '#fc8d59', '#d7301f'],
+              5: ['#fef0d9', '#fdcc8a', '#fc8d59', '#e34a33', '#b30000'],
+              6: ['#fef0d9', '#fdd49e', '#fdbb84', '#fc8d59', '#e34a33', '#b30000'],
+            },
           },
-        },
       colorSchemes: {
         sequential: [
           {
