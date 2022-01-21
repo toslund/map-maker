a5ef606ee57687e9e31b9f6206d714534c7b6c2f
toslund <timoslund@gmail.com>
2022-01-21 08:09:46 -0800

    remove vuetify link

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/toslund/map-maker"
         target="_blank"
         text
       >
